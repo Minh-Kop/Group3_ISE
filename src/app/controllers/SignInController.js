539276d716a6dd54b5380435ae7bde8fb9d9296e
nlduy2402 <duyl8972@gmail.com>
2023-01-07 02:17:19 +0700

    sign-in
    check đc username, chưa check password

--- src/app/controllers/SignInController.js
+++ src/app/controllers/SignInController.js
@@ -1,10 +1,26 @@
+const User = require('../models/User');
+const { mongooseToObject } = require('../../util/mongoose');
 //! đăng nhập
 class SignInController {
     //get /sign-up
     index(req, res) {
         res.render('sign-in');
     }
-    
+    check(req, res) {
+        var usr = req.body.usrname;
+        var pwd = req.body.password;
+        User.findOne({$or: [{usrname:usr}]})
+        .then(user => {
+            if(user) {
+                res.render('home');
+                
+            }
+            else {
+                res.json({
+                    message: 'no user found !'
+                })
+            }
+        })
 }
-
+}
 module.exports = new SignInController();
--- src/routes/sign-in.js
+++ src/routes/sign-in.js
@@ -3,5 +3,5 @@ const router = express.Router();
 
 const signInController = require('../app/controllers/SignInController');
 router.get('/', signInController.index);
-
+router.post('/check',signInController.check)
 module.exports = router;
